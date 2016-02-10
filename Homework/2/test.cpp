3b248772d668a972015a1b6a88b7d1c62692b160
Ryan Krattiger <rjk9w5@mst.edu>
2016-02-10 11:59:21 -0600

    small updates

--- Homework/2/test.cpp
+++ Homework/2/test.cpp
@@ -18,33 +18,41 @@ int main()
   fin >> ninputs;
   for(int i=0; i < ninputs; ++i)
   {
+    // Check for end of file (in case the input number was too big)
+    if(fin.get() && fin.eof()) break;
+    fin.unget(); // if the eof was not found, unget the next value
+
     fin >> p;
     p_vec.push_back(p);
   }
-
-  //std::cin >> p_input;
-
-  //std::cout << p_input(3) << '\n';
-
+  // Evaluate each polynomial input
   for(auto it: p_vec)
   {
     std::cout << "f(2) = " << it << " = "<< it(2) << '\n';
   }
 
+  // Get some monomial from a polynomial
   m = p_vec[0][3];
-
+  // And evaluate that monomial
   std::cout << m(1) << '\n';
 
+  // Test a bunch of operators
   p = p_vec[1];
+  // +=
   p += p;
   std::cout << p << '\n';
+  //-= and +
   p -= p_vec[0];
   p = p_vec[1] + p;
   std::cout << p << '\n';
 
+  // Unary -
   std::cout << -p << '\n';
+  // Unary ~
   std::cout << ~p << '\n';
+  // +
   std::cout << ~p + p << '\n';
+  // -
   std::cout << ~p - p << '\n';
 
 
